cc63c69731a054531aa8b78f09d9ed7127a66ae5
karthikkm007 <jacksparrow941994@gmail.com>
2018-08-10 14:05:53 +0530

    rename 8909 >> 8916

--- Android.mk
+++ Android.mk
@@ -8,11 +8,11 @@ else ifneq ($(filter msm8x26,$(TARGET_BOARD_PLATFORM)),)
   include $(call all-named-subdir-makefiles,msm8226)
 else ifneq ($(filter msm8992,$(TARGET_BOARD_PLATFORM)),)
   include $(call all-named-subdir-makefiles,msm8994)
-else ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
+else ifneq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
   ifeq ($(TARGET_SUPPORTS_QCOM_3100),true)
     include $(call all-named-subdir-makefiles,msm8909w_3100)
   else
-    include $(call all-named-subdir-makefiles,msm8909)
+    include $(call all-named-subdir-makefiles,msm8916)
   endif
 else ifneq ($(wildcard $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)),)
   include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
